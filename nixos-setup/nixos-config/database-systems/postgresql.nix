ea80d19699b39bbfa00a192c268890cf2b851303
dezly-macauley <dezlymacauley@proton.me>
2024-11-14 14:52:07 +0200

    Updated postgresql config localhost connections now work

--- nixos-setup/nixos-config/database-systems/postgresql.nix
+++ nixos-setup/nixos-config/database-systems/postgresql.nix
@@ -8,27 +8,43 @@
     # This is a service block that tells NixOs that everything inside
     # the curly braces is a related to PostgreSQL settings 
 
+
+
     services.postgresql = {
+
+        # This will set the version of PostgreSQL to version 16 
+        package = pkgs.postgresql_16;
 
         # This will ensure that PostgreSQL is running in the background
         enable = true;
 
-        # This will make sure that when PostgreSQL is launched, it will
-        # start with a database called `ground-zero`
-        ensureDatabases = [ "ground-zero" ];
+        # Enable TCIP connections
+        enableTCPIP = true;
+
+        # NOTE: PostgreSQL will use `port = 5432;` by default
 
         # This is a special function in NixOS that lets you override 
         # the default settings of a service with a high priority.
         # The higher the number, the better, but 10 is a reasonable number.
         authentication = pkgs.lib.mkOverride 10 ''
-            # This is where you specify what authentication method is
-            # required when a user needs to access a database, and what
-            # connections, user access, and database access this rule 
-            # should apply.
-            # For a production setup, do NOT leave the auth-method as trust!
-            # connection-type   databases  users      auth-method
-              local             all        all        trust
+        # This is where you specify what authentication method is
+        # required when a user needs to access a database, and what
+        # connections, user access, and database access this rule 
+        # should apply:
+        # ------------------------------------------------------------
+        # This will allow "host" based authentification only from other 
+        # webservices on the same computer ("127.0.0.1"), 
+        # although any DB user will have access to any database.
+        # ------------------------------------------------------------
+        #   type       database     DBuser  origin-address auth-method
+            local      all          all     trust
+        # (ipv4 connections)
+            host        all         all     127.0.0.1/32   trust
+        # (ipv6 connections)
+            host        all         all     ::1/128        trust
         '';
+        # ------------------------------------------------------------
+
     };
 
     # NOTE: Additional Packages
