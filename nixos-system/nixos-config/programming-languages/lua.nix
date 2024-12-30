82a5be4e6f5033059c60ea51877f0225c2cddc58
Dezly Macauley <dezlymacauley@proton.me>
2024-12-30 15:11:42 +0200

    Update lua.nix

--- nixos-system/nixos-config/programming-languages/lua.nix
+++ nixos-system/nixos-config/programming-languages/lua.nix
@@ -8,14 +8,8 @@
             # Powerful, fast, lightweight, embeddable scripting language
             lua
 
-            # A high performance JIT compiler for Lua
-            luajit
-           
-			# The Lua package manager
-			luajitPackages.luarocks
-
-			# Language server
-			lua-language-server			
+	    # Language server
+	    lua-language-server			
            
             # Syntax highlighting
             vimPlugins.nvim-treesitter-parsers.lua
